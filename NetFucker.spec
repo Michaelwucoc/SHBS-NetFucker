f4408b93a4ffb8cd1ad84da2bc990c0265c9f03f
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2025-03-06 10:14:28 +0800

    好的没修好

--- NetFucker.spec
+++ NetFucker.spec
@@ -11,7 +11,6 @@ datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
 tmp_ret = collect_all('urllib3')
 datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
 
-
 a = Analysis(
     ['netfucker.py'],
     pathex=[],
@@ -22,15 +21,18 @@ a = Analysis(
     hooksconfig={},
     runtime_hooks=[],
     excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=None,
     noarchive=False,
-    optimize=0,
 )
-pyz = PYZ(a.pure)
+pyz = PYZ(a.pure, a.zipped_data, cipher=None)
 
 exe = EXE(
     pyz,
     a.scripts,
     a.binaries,
+    a.zipfiles,
     a.datas,
     [],
     name='NetFucker',
@@ -39,17 +41,11 @@ exe = EXE(
     strip=False,
     upx=True,
     upx_exclude=[],
-    runtime_tmpdir=None,
+    runtime_tmpdir='.',
     console=False,
     disable_windowed_traceback=False,
-    argv_emulation=False,
-    target_arch=None,
+    target_arch='x86_64',
     codesign_identity=None,
     entitlements_file=None,
-)
-app = BUNDLE(
-    exe,
-    name='NetFucker.app',
-    icon=None,
-    bundle_identifier=None,
+    cross_arch=True,
 )
--- build.py
+++ build.py
@@ -58,10 +58,11 @@ def build_app():
             '--name=NetFucker',
             '--noconsole',
             '--onefile',
-            '--add-data=requirements.txt:.',
+            f'--add-data=requirements.txt{os.pathsep}.',
             '--collect-all=tkinter',
             '--collect-all=requests',
             '--collect-all=urllib3',
+            '--runtime-tmpdir=.',
             'netfucker.py'
         ]
         
@@ -79,6 +80,71 @@ def build_app():
             icon_path = root_dir / 'icon.ico'
             if icon_path.exists():
                 pyinstaller_args.extend(['--icon=' + str(icon_path)])
+            # 添加Windows特定选项
+            if platform.system() != 'Windows':
+                # 在非Windows系统上构建Windows版本
+                pyinstaller_args.extend(['--target-arch=x86_64'])
+                # 设置环境变量以支持交叉编译
+                os.environ['PYTHONPATH'] = os.pathsep.join([
+                    os.environ.get('PYTHONPATH', ''),
+                    os.path.join(os.path.dirname(sys.executable), 'lib', 'python' + sys.version[:3], 'site-packages')
+                ])
+                os.environ['PYTHON_ARCH'] = 'x86_64'
+                # 添加spec文件配置以支持Windows构建
+                spec_content = '''# -*- mode: python ; coding: utf-8 -*-
+from PyInstaller.utils.hooks import collect_all
+
+datas = [('requirements.txt', '.')]
+binaries = []
+hiddenimports = []
+tmp_ret = collect_all('tkinter')
+datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
+tmp_ret = collect_all('requests')
+datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
+tmp_ret = collect_all('urllib3')
+datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
+
+a = Analysis(
+    ['netfucker.py'],
+    pathex=[],
+    binaries=binaries,
+    datas=datas,
+    hiddenimports=hiddenimports,
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=None,
+    noarchive=False,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=None)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name='NetFucker',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir='.',
+    console=False,
+    disable_windowed_traceback=False,
+    target_arch='x86_64',
+    codesign_identity=None,
+    entitlements_file=None,
+    cross_arch=True,
+)'''
+                spec_file = root_dir / 'NetFucker.spec'
+                spec_file.write_text(spec_content.strip())
+                pyinstaller_args = ['pyinstaller', 'NetFucker.spec']
         
         # 执行构建
         print(f'正在构建 {platform_name} 版本...')
